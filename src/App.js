50c671a34742db9cc50192a59cf1cce7531c0b56
mbaxret2 <emily.turner@manchester.ac.uk>
2020-01-17 12:01:52 +0000

    adding app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Router } from '@reach/router'
-import Header from './components/Header';
 import Home from './components/Home';
-import Topics from './components/Topics';
-import Topic from './components/Topic';
-import Article from './components/Article';
-import Comments from './components/Comments';
+import Topics from './components/topics/Topics';
+import Topic from './components/topics/Topic';
+import Article from './components/articles/Article';
 import NavBar from './components/NavBar';
+import ErrorDisplay from './components/ErrorDisplay';
+// import LoginPage from './components/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <NavBar />
-      <Router className='route'>
-        <Home path='/' />
-        <Topics path='/topics' />
-        <Topic path='/topics/:topic' />
-        <Article path='/article/:article_id' />
-        <Comments path='/article/:article_id/comments' />
-      </Router>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loggedIn: false,
+    user: ''
+  }
+  render () {
+    const {user, loggedIn} = this.state
+    return (
+      <div className="App">
+        <NavBar user={user} logUserIn={this.logUserIn}/>
+        {/* <Header /> */}
+        <Router className='route' >
+          <Home path='/' loggedIn={loggedIn}/>
+          <Topics path='/topics' loggedIn={loggedIn}/>
+          {/* <LoginPage path='/login' logUserIn={this.logUserIn} msg='hi' /> */}
+          <Topic path='/topics/:topic' loggedIn={loggedIn}/>
+          <Article path='/topics/:topic/:article_id' loggedIn={loggedIn} user={user}/>
+          <ErrorDisplay default status={404} msg='Non-existent Path' loggedIn={loggedIn}/>
+        </Router>
+      </div>
+    );
 
+  }
+
+  logUserIn = () => {
+    console.log('clicked!')
+    this.setState({ loggedIn: true, user: 'jessjelly' })
+    return <Home />
+  }
 }
 
 export default App;
