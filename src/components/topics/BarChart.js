e10a76458748fc9d3c5957ed975a0d38cbc39217
mbaxret2 <emily.turner@manchester.ac.uk>
2020-01-20 08:11:25 +0000

    d3 with fake data

---
+++ src/components/topics/BarChart.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3'
+// import * as api from '../api';
+import * as fakeData from './fakeData'
+
+class BarChart extends Component {
+    
+
+    componentDidMount() {
+        const { topic } = this.props;
+        const data = fakeData[topic]
+        console.log('ths data', data)
+        this.drawBarChart(data)
+    }
+    render() { 
+        return <div ref="canvas5"></div> 
+    }
+    drawBarChart(data)  {
+        
+        const margin = {top: 10, right: 30, bottom: 90, left: 40},
+        width = 450,
+        height = 85;
+               
+            const scale = 2
+            const svgTopics = d3.select(this.refs.canvas5)
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .style('border-bottom', '1px solid black')
+                // .append('g')
+                // .attr('transform', 'translate(' + margin.left + ', ', + margin.top + ')')
+            
+            // x-axis
+            // const x = d3.scaleBand()
+            //   .range([0, width])
+            //   .domain(data.map(d => {return d.xLabels}))
+            //   .padding(0.2)
+
+
+
+            svgTopics.selectAll('rect')
+                .data(data)
+                .enter()
+                .append('rect')
+                    .attr('x', (d, iteration) => iteration * 15)
+                    .attr('width', 15)
+                    .attr('fill', 'orange')
+                    .attr('height', (d) => 0)
+                    .attr('y', (d) => 0)
+
+            svgTopics.selectAll('rect')
+              .transition()
+              .duration(800)
+              .attr('y', (d) => height - d * scale)
+              .attr('height', (d) => height * scale)
+              .delay((d, i) => {console.log(i); return i * 100})
+                }
+}
+export default BarChart
--- src/components/topics/TopicCard.js
+++ src/components/topics/TopicCard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from '@reach/router'
 import { capitalize } from '../utils/utils'
+import BarChart from './BarChart';
 
 const TopicCard = ({ topic }) => {
   const { slug, description } = topic;
   return (
-    <li>
+    <li className='topic-card'>
       <Link to={`/topics/${slug}`}>
-        <p><span className='text-span'>{capitalize(slug)}</span> - {description}</p>
+        <p className='left'><span className='text-span'>{capitalize(slug)}</span> - {description}</p>
         <p></p>
 
       </Link>
+      <BarChart topic={slug} />
     </li>
   );
 };
